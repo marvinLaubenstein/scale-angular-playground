6934a348585d32817eedfff2366f3dad1b9f56d8
marvinLaubenstein <marvin.laubenstein@telekom.de>
2022-05-18 09:23:31 +0200

    feat: adding test for displaying notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Scale + Angular';
+  display = false;
+  onPress() {
+    this.display = true;
+    /*if you want the component to show and hide on click pressed, use 
+    use this line
+    this.display = !this.display;*/
+  }
 }
