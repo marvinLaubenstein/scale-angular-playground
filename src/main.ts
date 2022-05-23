80d12ec2585a3d81227e8d3f63885de0b0bf8751
marvinLaubenstein <marvin.laubenstein@telekom.de>
2022-05-23 18:49:01 +0200

    feat: adding working test for notification toast

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { defineCustomElements } from '@telekom/scale-components/loader';
+import { defineCustomElements } from 'node_modules/@telekom/scale-components/loader';
 
 if (environment.production) {
   enableProdMode();
